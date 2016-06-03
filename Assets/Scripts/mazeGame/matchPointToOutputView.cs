2e034aa700f08388118c1c0ccb99b4a613e401ff
L.Guo <iml885203@gmail.com>
2016-06-03 20:14:28 +0800

    成功對應轉換座標 尚未做實體測試

--- Assets/Scripts/mazeGame/matchPointToOutputView.cs
+++ Assets/Scripts/mazeGame/matchPointToOutputView.cs
@@ -8,11 +8,16 @@ public class matchPointToOutputView : MonoBehaviour {
     public GameObject _mapobject;
     private int width;
     private int height;
+    private clickPositionTrans _clickPositionTrans;
+    public double OutputX { get; private set; }
+    public double OutputY { get; private set; }
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
+
+    // Use this for initialization
+    void Start () {
+        OutputX = 0.0;
+        OutputY = 0.0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,7 +25,9 @@ public class matchPointToOutputView : MonoBehaviour {
         {
             showData();
         }
-	}
+        
+
+    }
     
     void showData()
     {
@@ -29,15 +36,22 @@ public class matchPointToOutputView : MonoBehaviour {
             width = _MatchManager.Width;
             height = _MatchManager.Height;
             Debug.Log("Block Size: " + width + " x " + height);
+            Debug.Log("object Size: " + _mapobject.transform.localScale.x + " x " + _mapobject.transform.localScale.y);
+            _clickPositionTrans = new clickPositionTrans(width, height, _mapobject.transform.localScale.x, _mapobject.transform.localScale.y);
         }
         
         Point[] objectPoint = _MatchManager.MatchObjectPoint.ToArray();
         
             
-        for(int i=0; i<objectPoint.Length; i++)
+        for(int i=0; i<objectPoint.Length; i=i+2)
         {
-            Debug.Log("#" + i + ": " + objectPoint[i].x + "," + objectPoint[i].y);
-
+            var centerX = (objectPoint[0].x + objectPoint[1].x) / 2;
+            var centerY = (objectPoint[0].y + objectPoint[1].y) / 2;
+            //Debug.Log("#" + (i + 2)/2 + ": " + centerX + "," + centerY);
+            var transPoint = _clickPositionTrans.TransToScreen2Pos(new Point(centerX, centerY));
+            //Debug.Log("object" + ": " + transPoint.x + "," + transPoint.y);
+            OutputX = transPoint.x;
+            OutputY = transPoint.y;
         }
     }
 }
