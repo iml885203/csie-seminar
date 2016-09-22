51c66636bc4737943b35f7d1d809450d5f116461
LAB-PC\LAB <a07458666@gmail.com>
2016-09-22 23:41:40 +0800

    新增黑洞(wind)會吸雷射 以及可以折射的牆壁 mapData設置關卡物件 讀取objectObject文字檔

--- Assets/LaserGame/scripts/LaserBall.cs
+++ Assets/LaserGame/scripts/LaserBall.cs
@@ -3,66 +3,121 @@ using System.Collections;
 
 public class LaserBall : MonoBehaviour {
     private LaserGenerator _generator;
+    private Rigidbody _ballRigidbody;
     private int _reflectCount;
-	// Use this for initialization
-	void Start () {
+    private Vector3 _beforeReflectVelocity;
+
+    public WindZone _wind;
+    Vector3 _windForce;
+    float _windRadius;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _windForce = _wind.GetComponent<Wind>().GetWindForce();
+        _windRadius = _wind.GetComponent<Wind>().GetWindRadius();
+
+        //_reflashVelocityFlag = true;
+        _ballRigidbody = GetComponent<Rigidbody>();
         _generator = gameObject.GetComponentInParent<LaserGenerator>();
         _reflectCount = 0;
     }
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(this.gameObject.transform.position, _wind.transform.position) < _windRadius)
+        {
+            //Vector3 windForce = new Vector3((this.gameObject.transform.position.x - _wind.transform.position.x),
+            //                            (this.gameObject.transform.position.y - _wind.transform.position.y),
+            //                            0);
+
+            Vector3 windForce = new Vector3((_wind.transform.position.x - this.gameObject.transform.position.x) * 100,
+                                            (_wind.transform.position.y - this.gameObject.transform.position.y) * 100,
+                                            0);
+
+            //windForce = windForce.normalized;
+            //Debug.Log("windForce = " + windForce);
+            this.GetComponent<ConstantForce>().force = windForce;
+        }
+        else
+        {
+            this.GetComponent<ConstantForce>().force = Vector3.zero;
+        } 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.Log("Point of contact: " + hit.point);
+        }
+
+        if (other.gameObject.tag == "refractionObjects")
+        {
+            Vector3 beforeReflectVelocity;
+            Vector3 afterReflectVelocity;
+
+            beforeReflectVelocity = _ballRigidbody.velocity;
+            afterReflectVelocity = Vector3.Reflect(_ballRigidbody.velocity, hit.normal);
+
+            Debug.Log("beforeReflectVelocity.velocity = " + beforeReflectVelocity);
+            Debug.Log("afterReflectVelocity.velocity = " + afterReflectVelocity);
+
+            Vector3 horizontaVector = new Vector3((beforeReflectVelocity.x + afterReflectVelocity.x) / 2, (beforeReflectVelocity.y + afterReflectVelocity.y) / 2, 0);
+
+            _ballRigidbody.velocity = new Vector3((horizontaVector.x + beforeReflectVelocity.x) / 2, (horizontaVector.y + beforeReflectVelocity.y) / 2, 0);
+        }
+    }
 
     void OnCollisionEnter(Collision other)
     {
+        
         ContactPoint contact = other.contacts[0];
-        Vector3 nowPosition = contact.point;
+        Vector3 reflectPosition = contact.point;
+        //Debug.Log("reflectPosition = " + reflectPosition);
+
         if (other.gameObject.tag == "wall") //碰撞到牆壁
         {
-            _generator.SetLaserLinesPos(_reflectCount, 1, nowPosition);
+            _generator.SetLaserLinesPos(_reflectCount, 1, reflectPosition);
             _generator.HideNotUseLaser(_reflectCount);
             Destroy(this.gameObject);
         }
         else if(other.gameObject.tag == "reflectionObject") //碰撞反射物體
         {
             _reflectCount++;
-            _generator.SetLaserLinesPos(_reflectCount - 1, 1, nowPosition);
-            _generator.SetLaserLinesPos(_reflectCount, 0, nowPosition);
+            _generator.SetLaserLinesPos(_reflectCount - 1, 1, reflectPosition);
+            _generator.SetLaserLinesPos(_reflectCount, 0, reflectPosition);
             //Debug.Log("_reflectCount = " + _reflectCount + ", nowPosition = " + nowPosition + "."); 
         }
-        else if(other.gameObject.tag == "dispersionObject")
+        //else if (other.gameObject.tag == "dispersionObject")
+        //{
+        //    //修改物體速度(根據vector3)
+        //    //入射向量
+        //    Vector3 beforeReflectVelocity = other.relativeVelocity;
+        //    //反射向量
+        //    //Vector3 afterReflectVelocity = new Vector3(20, -1, 0);
+        //    //_ballRigidbody.velocity = afterReflectVelocity;
+        //    Debug.Log("reflectPosition = " + reflectPosition);
+        //    Debug.Log("beforeReflectVelocity.velocity = " + beforeReflectVelocity);
+        //    Debug.Log("afterReflectVelocity.velocity = " + _ballRigidbody.velocity);
+
+        //    //this.gameObject.transform.rotation = Quaternion.AngleAxis(20, Vector3.up);
+        //    //Debug.Log("after = " + this.gameObject.transform.rotation);
+        //    //cloneBall.transform.SetParent(this.transform.parent);
+        //}
+        else if (other.gameObject.tag == "targetObject")
         {
-            //Vector3 movementCurrentDirection = transform.TransformDirection(1, 1, 0);
-            //Vector3 reflectionDirection = Vector3.Reflect(movementCurrentDirection, nowPosition);
+            Debug.Log("Win");
+            Destroy(this.gameObject);
 
-            //Vector3 randomReflectDirection = Quaternion.Euler(0, 0, Random.Range(-80, 80)) * reflectionDirection;
-            //movementCurrentDirection = randomReflectDirection;
-            //Debug.Log("movementCurrentDirection = " + movementCurrentDirection);
-
-            Rigidbody myRigidbody = GetComponent<Rigidbody>();
-            Vector3 test = new Vector3(19, 6, 0);
-            Debug.Log("myRigidbody.velocity = " + myRigidbody.velocity);
-            myRigidbody.velocity = test;
-            //Vector3 velocity = myRigidbody.velocity;
-            //myRigidbody.velocity = Vector3.zero;
-            //StartCoroutine(Example());
-            //myRigidbody.velocity = velocity;
-
-            //Debug.Log("before = " + this.gameObject.transform.rotation);
-            //GameObject cloneBall = (GameObject)Instantiate(this.gameObject, this.transform.position, this.transform.rotation);
-
-            //this.gameObject.transform.rotation = Quaternion.AngleAxis(20, Vector3.up);
-            //Debug.Log("after = " + this.gameObject.transform.rotation);
-            //cloneBall.transform.SetParent(this.transform.parent);
         }
-    }
-
-    IEnumerator Example()
-    {
-        print(Time.time);
-        yield return new WaitForSeconds(5);
-        print(Time.time);
     }
 }
---
+++ Assets/LaserGame/scripts/Wind.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour
+{
+    private float _radius;
+    private Vector3 _force;
+    public GameObject ball;
+
+    void Start()
+    {
+        
+        _radius = 1;
+        _force = new Vector3(0, -10, 0);
+    }
+
+    void Update()
+    {
+        //Debug.Log("transform.parent = " + transform.parent.parent.ToString());
+        //ball = transform.parent.parent.FindChild("LaserGenerator").FindChild("laserBall(Clone)").gameObject;
+    }
+
+    public float GetWindRadius()
+    {
+        return _radius;
+    }
+
+    public Vector3 GetWindForce()
+    {
+        return _force;
+    }
+
+}
