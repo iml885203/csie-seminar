1176e0bea22870bdef08f4105bbb4b9f585dfaf5
Tyson382 <tyson13032303@gmail.com>
2016-10-02 20:10:25 +0800

    修改按鈕事件 scene

--- Assets/MagicCubeWorld/Scripts/ButtonEvent.cs
+++ Assets/MagicCubeWorld/Scripts/ButtonEvent.cs
@@ -6,12 +6,17 @@ using UnityEngine.UI;
 public class ButtonEvent : MonoBehaviour
 {
     //遊戲狀態
-    public int _stateFlag;
+    public GameStateIndex _gameStateIndex;
 
     //遊戲背景圖片
     public GameObject _menuBackGroundImage;
     public GameObject _listProducerBackGroundImage;
     public GameObject _settingBackGroundImage;
+
+    public GameObject[] _menuBackGroundImages;
+    public GameObject[] _settingBackGroundImages;
+    public GameObject[] _listProducerBackGroundImages;
+    public GameObject[] _gameRunBackGroundImages;
 
     //頁面按鈕
     public Button[] _inMenuButton;
@@ -28,12 +33,8 @@ public class ButtonEvent : MonoBehaviour
 
     public GameObject[] _levelPreviewImage;
 
-    //製作人顯示的離開與重設flag
-    private bool _exitFlage = true;
-    private bool _resetFlage = false;
-
-    //製作人顯示字幕及背景
-    public Text[] _textListProducer;
+    private float _effectTimer = 1f;
+    private float _readyTimer = 1f;
 
     void Start()
     {
@@ -42,31 +43,7 @@ public class ButtonEvent : MonoBehaviour
 
     void Update()
     {
-        this.RunListProducer();
-        //在字幕離開
-        if (_exitFlage && _resetFlage)
-        {
-            _listProducerBackGroundImage.SetActive(false);
-            _settingBackGroundImage.SetActive(true);
-
-            this.InMenuGameObjectEnableOrDisable(false);
-            this.InSettingGameObjectEnableOrDisable(true);
-            this.InGameRunGameObjectEnableOrDisable(false);
-            this.InListProducerObjectEnableOrDisable(false);
-
-            RectTransform backGroundRectTransform = _listProducerBackGroundImage.GetComponent<RectTransform>();
-
-            RectTransform temp = _textListProducer[0].transform as RectTransform;
-            temp.anchoredPosition = new Vector3(402, -102, 0);
-            temp = _textListProducer[1].transform as RectTransform;
-            temp.anchoredPosition = new Vector3(402, -302, 0);
-            temp = _textListProducer[2].transform as RectTransform;
-            temp.anchoredPosition = new Vector3(402, -502, 0);
-
-
-            _resetFlage = false;
-        }
-        _inSettingText[0].text = "背景音樂音量: " + _inSettingSoundSlider[0].value.ToString("0");
+        _inSettingText[0].text = "背景音量: " + _inSettingSoundSlider[0].value.ToString("0");
         _inSettingText[1].text = "音效音量: " + _inSettingSoundSlider[1].value.ToString("0");
 
 
@@ -75,18 +52,18 @@ public class ButtonEvent : MonoBehaviour
             if (index == int.Parse(_levelFlagText.text.ToString()) - 1)
             {
                 _inSettingLevelChoiceButton[index].interactable = false;
-                if (_levelFlagText.gameObject.activeSelf)
-                {
-                    _levelPreviewImage[index].SetActive(true);
-                }
+                //if (_levelFlagText.gameObject.activeSelf)
+                //{
+                //    _levelPreviewImage[index].SetActive(true);
+                //}
             }
             else
             {
                 _inSettingLevelChoiceButton[index].interactable = true;
-                if (_levelFlagText.gameObject.activeSelf)
-                {
-                    _levelPreviewImage[index].SetActive(false);
-                }
+                //if (_levelFlagText.gameObject.activeSelf)
+                //{
+                //    _levelPreviewImage[index].SetActive(false);
+                //}
             }
         }
     }
@@ -98,64 +75,112 @@ public class ButtonEvent : MonoBehaviour
         mi.Invoke(this, null);
     }
 
-    //從開始畫面到設定畫面按鈕事件(menu -> setting)
-    public void GameStateMenuToSettingButtonClick()
+    IEnumerator MenuToSetting()
     {
-        _stateFlag = GameState.Setting;
+        yield return new WaitForSeconds(_effectTimer);
+        _gameStateIndex.ToStateSetting();
+        this.SwitchBackGroundImagesByStateFlag();
+        //yield return new WaitForSeconds(_readyTimer);
         this.SwitchGameStateByStateFlag();
 
         _inSettingButton[1].GetComponentInChildren<Text>().text = "開始遊戲";
     }
 
-    //從設定畫面到遊戲執行按鈕事件(setting -> gameRun)
-    public void GameStateSettingToGameRunButtonClick()
+    //從開始畫面到設定畫面按鈕事件(menu -> setting)
+    public void GameStateMenuToSettingButtonClick()
     {
-        _stateFlag = GameState.GameRun;
+        StartCoroutine(MenuToSetting());
+    }
+
+    IEnumerator SettingToGameRun()
+    {
+        yield return new WaitForSeconds(_effectTimer);
+        _gameStateIndex.ToStateGameRun();
+        this.SwitchBackGroundImagesByStateFlag();
+        //yield return new WaitForSeconds(_readyTimer);
         this.SwitchGameStateByStateFlag();
 
         _inMenuButton[0].GetComponentInChildren<Text>().text = "設定";
 
-        ViewActiveEnableOrDisable(true);
+        //ViewActiveEnableOrDisable(true);
+    }
+
+    //從設定畫面到遊戲執行按鈕事件(setting -> gameRun)
+    public void GameStateSettingToGameRunButtonClick()
+    {
+        StartCoroutine(SettingToGameRun());
+    }
+
+    IEnumerator GameRunToSetting()
+    {
+        yield return new WaitForSeconds(_effectTimer);
+        _gameStateIndex.ToStateSetting();
+        this.SwitchBackGroundImagesByStateFlag();
+        //yield return new WaitForSeconds(_readyTimer);
+        this.SwitchGameStateByStateFlag();
+
+        _inSettingButton[1].GetComponentInChildren<Text>().text = "繼續遊戲";
+
+        //this.ViewActiveEnableOrDisable(false);
     }
 
     //從遊戲執行到設定畫面按鈕事件(gameRun -> setting)
     public void GameStateGameRunToSettingButtonClick()
     {
-        _stateFlag = GameState.Setting;
-        this.SwitchGameStateByStateFlag();
-
-        _inSettingButton[1].GetComponentInChildren<Text>().text = "繼續遊戲";
-
-        this.ViewActiveEnableOrDisable(false);
+        StartCoroutine(GameRunToSetting());
     }
 
-    //從設定畫面到開始畫面按鈕事件(setting -> menu)
-    public void GameStateSettingToMenuButtonClick()
+    IEnumerator SettingToMenu()
     {
-        _stateFlag = GameState.Menu;
+        yield return new WaitForSeconds(_effectTimer);
+        _gameStateIndex.ToStateMenu();
+        this.SwitchBackGroundImagesByStateFlag();
+        //yield return new WaitForSeconds(_readyTimer);
         this.SwitchGameStateByStateFlag();
 
         _inMenuButton[0].GetComponentInChildren<Text>().text = "進入設定並開始遊戲";
     }
 
+    //從設定畫面到開始畫面按鈕事件(setting -> menu)
+    public void GameStateSettingToMenuButtonClick()
+    {
+        StartCoroutine(SettingToMenu());
+    }
+
+    IEnumerator SettingToListProducer()
+    {
+        yield return new WaitForSeconds(_effectTimer);
+        _gameStateIndex.ToStateListProducer();
+        this.SwitchBackGroundImagesByStateFlag();
+        //yield return new WaitForSeconds(_readyTimer);
+        this.SwitchGameStateByStateFlag();
+
+        //_exitFlage = false;
+        //_listProducerBackGroundImage.SetActive(true);
+    }
+
     //按下製作人按鈕事件
     public void GameStateSettingToListProducerButtonClick()
     {
-        _stateFlag = GameState.ListProducer;
+        StartCoroutine(SettingToListProducer());
+    }
+
+    IEnumerator ListProducerToSetting()
+    {
+        yield return new WaitForSeconds(_effectTimer);
+        _gameStateIndex.ToStateSetting();
+        this.SwitchBackGroundImagesByStateFlag();
+        //yield return new WaitForSeconds(_readyTimer);
         this.SwitchGameStateByStateFlag();
 
-        _exitFlage = false;
-        _listProducerBackGroundImage.SetActive(true);
+        //_resetFlage = true;
+        //_exitFlage = true;
     }
 
     //按下回設定事件(在按下製作人後出現的按鈕)
     public void GameStateListProducerToSettingButtonClick()
     {
-        _stateFlag = GameState.Setting;
-        this.SwitchGameStateByStateFlag();
-
-        _resetFlage = true;
-        _exitFlage = true;
+        StartCoroutine(ListProducerToSetting());
     }
 
     public void ChoiceLevel1ButtonClick()
@@ -173,66 +198,39 @@ public class ButtonEvent : MonoBehaviour
         _levelFlagText.text = "3";
     }
 
-    //字幕移動
-    private void RunListProducer()
+    public void SwitchGameStateByStateFlag()
     {
-        //當最後一個字幕經過指定條件後，則進行一次重設字幕
-        if (_textListProducer[2].transform.position.y >= -3)
-        {
-            _resetFlage = true;
-        }
-
-        //移動主程式
-        if (_textListProducer[2].transform.position.y < 30 && !_exitFlage)//_textListProducer[2].transform.position.y < 300 ||
-        {
-            _textListProducer[0].transform.Translate(0, 0.5f, 0);
-            _textListProducer[1].transform.Translate(0, 0.5f, 0);
-            _textListProducer[2].transform.Translate(0, 0.5f, 0);
-        }
-        else
-        {
-            //判定為字幕已離開，並將回設定的按鈕取消active
-            _exitFlage = true;
-            for (int index = 0; index < _inListProducerButton.Length; index++)
-            {
-                _inListProducerButton[index].gameObject.SetActive(false);
-            }
-        }
-    }
-
-    private void SwitchGameStateByStateFlag()
-    {
-        switch (_stateFlag)
+        switch (_gameStateIndex.GetCurrentGameStateIndex())
         {
             case GameState.Menu:
                 {
                     this.InMenuGameObjectEnableOrDisable(true);
-                    this.InSettingGameObjectEnableOrDisable(false);
-                    this.InGameRunGameObjectEnableOrDisable(false);
-                    this.InListProducerObjectEnableOrDisable(false);
+                    this.InSettingGameObjectEnableOrDisable(true);
+                    this.InGameRunGameObjectEnableOrDisable(true);
+                    this.InListProducerObjectEnableOrDisable(true);
                     break;
                 }
             case GameState.Setting:
                 {
-                    this.InMenuGameObjectEnableOrDisable(false);
+                    this.InMenuGameObjectEnableOrDisable(true);
                     this.InSettingGameObjectEnableOrDisable(true);
-                    this.InGameRunGameObjectEnableOrDisable(false);
-                    this.InListProducerObjectEnableOrDisable(false);
+                    this.InGameRunGameObjectEnableOrDisable(true);
+                    this.InListProducerObjectEnableOrDisable(true);
                     break;
                 }
             case GameState.GameRun:
                 {
-                    this.InMenuGameObjectEnableOrDisable(false);
-                    this.InSettingGameObjectEnableOrDisable(false);
+                    this.InMenuGameObjectEnableOrDisable(true);
+                    this.InSettingGameObjectEnableOrDisable(true);
                     this.InGameRunGameObjectEnableOrDisable(true);
-                    this.InListProducerObjectEnableOrDisable(false);
+                    this.InListProducerObjectEnableOrDisable(true);
                     break;
                 }
             case GameState.ListProducer:
                 {
-                    this.InMenuGameObjectEnableOrDisable(false);
-                    this.InSettingGameObjectEnableOrDisable(false);
-                    this.InGameRunGameObjectEnableOrDisable(false);
+                    this.InMenuGameObjectEnableOrDisable(true);
+                    this.InSettingGameObjectEnableOrDisable(true);
+                    this.InGameRunGameObjectEnableOrDisable(true);
                     this.InListProducerObjectEnableOrDisable(true);
                     break;
                 }
@@ -256,7 +254,7 @@ public class ButtonEvent : MonoBehaviour
             _inMenuButton[index].gameObject.SetActive(value);
         }
 
-        _menuBackGroundImage.SetActive(value);
+        //_menuBackGroundImage.SetActive(value);
     }
 
     //Setting畫面物件顯示與否
@@ -287,14 +285,14 @@ public class ButtonEvent : MonoBehaviour
         }
 
         //setting的選關預覽圖
-        for (int index = 0; index < _levelPreviewImage.Length; index++)
-        {
-            _levelPreviewImage[index].gameObject.SetActive(value);
-        }
+        //for (int index = 0; index < _levelPreviewImage.Length; index++)
+        //{
+        //    _levelPreviewImage[index].gameObject.SetActive(value);
+        //}
 
         _levelFlagText.gameObject.SetActive(value);
 
-        _settingBackGroundImage.SetActive(value);
+        //_settingBackGroundImage.SetActive(value);
     }
 
     //gameRun畫面物件顯示與否
@@ -316,12 +314,97 @@ public class ButtonEvent : MonoBehaviour
             _inListProducerButton[index].gameObject.SetActive(value);
         }
 
-        _listProducerBackGroundImage.SetActive(value);
+        //_listProducerBackGroundImage.SetActive(value);
     }
 
-    //顯示提示圖示的副程式
-    private void ViewActiveEnableOrDisable(bool value)
+    //////////////////////////////////////////////////////
+    /// 背景影像群處理
+    //////////////////////////////////////////////////////
+
+    //menu的背景圖片群顯示與否
+    private void MenuBackGroundImagesEnableOrDisable(bool value)
     {
-        //遊戲UI狀態設定
+        //menu的背景圖片群
+        for (int index = 0; index < _menuBackGroundImages.Length; index++)
+        {
+            _menuBackGroundImages[index].gameObject.SetActive(value);
+        }
+    }
+
+    //setting的背景圖片群顯示與否
+    private void SettingBackGroundImagesEnableOrDisable(bool value)
+    {
+        //setting的背景圖片群
+        for (int index = 0; index < _settingBackGroundImages.Length; index++)
+        {
+            _settingBackGroundImages[index].gameObject.SetActive(value);
+        }
+    }
+
+    //gameRun的背景圖片群顯示與否
+    private void GameRunBackGroundImagesEnableOrDisable(bool value)
+    {
+        //listProducer的背景圖片群
+        for (int index = 0; index < _gameRunBackGroundImages.Length; index++)
+        {
+            _gameRunBackGroundImages[index].gameObject.SetActive(value);
+        }
+    }
+
+    //listProducer的背景圖片群顯示與否
+    private void ListProducerBackGroundImagesEnableOrDisable(bool value)
+    {
+        //listProducer的背景圖片群
+        for (int index = 0; index < _listProducerBackGroundImages.Length; index++)
+        {
+            _listProducerBackGroundImages[index].gameObject.SetActive(value);
+        }
+    }
+
+    public void SwitchBackGroundImagesByStateFlag()
+    {
+        switch (_gameStateIndex.GetCurrentGameStateIndex())
+        {
+            case GameState.Menu:
+                {
+                    this.MenuBackGroundImagesEnableOrDisable(true);
+                    this.SettingBackGroundImagesEnableOrDisable(false);
+                    this.GameRunBackGroundImagesEnableOrDisable(false);
+                    this.ListProducerBackGroundImagesEnableOrDisable(false);
+                    break;
+                }
+            case GameState.Setting:
+                {
+                    this.MenuBackGroundImagesEnableOrDisable(false);
+                    this.SettingBackGroundImagesEnableOrDisable(true);
+                    this.GameRunBackGroundImagesEnableOrDisable(false);
+                    this.ListProducerBackGroundImagesEnableOrDisable(false);
+                    break;
+                }
+            case GameState.GameRun:
+                {
+                    this.MenuBackGroundImagesEnableOrDisable(false);
+                    this.SettingBackGroundImagesEnableOrDisable(false);
+                    this.GameRunBackGroundImagesEnableOrDisable(true);
+                    this.ListProducerBackGroundImagesEnableOrDisable(false);
+                    break;
+                }
+            case GameState.ListProducer:
+                {
+                    this.MenuBackGroundImagesEnableOrDisable(false);
+                    this.SettingBackGroundImagesEnableOrDisable(false);
+                    this.GameRunBackGroundImagesEnableOrDisable(false);
+                    this.ListProducerBackGroundImagesEnableOrDisable(true);
+                    break;
+                }
+            default:
+                {
+                    this.MenuBackGroundImagesEnableOrDisable(false);
+                    this.SettingBackGroundImagesEnableOrDisable(false);
+                    this.GameRunBackGroundImagesEnableOrDisable(false);
+                    this.ListProducerBackGroundImagesEnableOrDisable(false);
+                    break;
+                }
+        }
     }
 }
