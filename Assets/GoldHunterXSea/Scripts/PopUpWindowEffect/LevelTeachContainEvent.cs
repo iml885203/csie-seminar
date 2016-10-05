16200644db71a658d73801556e6c3d7749cd8e1e
Tyson382 <tyson13032303@gmail.com>
2016-10-05 18:01:51 +0800

    彈跳視窗內事件(根據關卡換視窗內容,下一步上一步)已完成 彈跳視窗差實裝彈跳動畫 skip按鈕事件 第一關部分及第二第三關內容圖片 實裝上關卡

---
+++ Assets/GoldHunterXSea/Scripts/PopUpWindowEffect/LevelTeachContainEvent.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using OpenCVForUnity;
+using UnityEngine.UI;
+using System;
+
+public class LevelTeachContainEvent : MonoBehaviour
+{
+    public LevelTeachState _teachState;
+    public Button _nextButton;
+    public Button _previousButton;
+    public Button _skipButton;
+    public GameObject[] _image;
+    public GameObject[] _description;
+
+    private int _stateCount;
+
+    void Start()
+    {
+        _stateCount = _image.Length;
+        this.SetCurrentContainActiveOrNot();
+        this.SetCurrentButtonEnableOrNot();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public void ToNextTeachStateEvent()
+    {
+        _teachState.ToNextState();
+        this.SetCurrentContainActiveOrNot();
+        this.SetCurrentButtonEnableOrNot();
+    }
+
+    public void ToPreviousTeachStateEvent()
+    {
+        _teachState.ToPreviousState();
+        this.SetCurrentContainActiveOrNot();
+        this.SetCurrentButtonEnableOrNot();
+    }
+
+    public void SetCurrentContainActiveOrNot()
+    {
+        for(int index = 0; index < _stateCount; index++)
+        {
+            if(_teachState.CurrentTeachState == index)
+            {
+                _image[index].SetActive(true);
+                _description[index].SetActive(true);
+            }
+            else
+            {
+                _image[index].SetActive(false);
+                _description[index].SetActive(false);
+            }
+        }
+    }
+
+    public void SetCurrentButtonEnableOrNot()
+    {
+        _previousButton.interactable = true;
+        _nextButton.interactable = true;
+
+        if (_teachState.CurrentTeachState == 0)
+            _previousButton.interactable = false;
+        if (_teachState.CurrentTeachState == _stateCount - 1)
+            _nextButton.interactable = false;
+    }
+}
+
+
