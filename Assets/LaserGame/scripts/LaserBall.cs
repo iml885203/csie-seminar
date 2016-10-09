ffcf374ddb07f777651b12d3b8c6b709a4fb2d33
Tyson382 <tyson13032303@gmail.com>
2016-10-09 16:44:51 +0800

    分離器功能完成

--- Assets/LaserGame/scripts/LaserBall.cs
+++ Assets/LaserGame/scripts/LaserBall.cs
@@ -97,9 +97,21 @@ public class LaserBall : MonoBehaviour {
                 //入射向量
                 Vector3 originVelocity = _ballRigidbody.velocity;
 
-                
+                Vector3 verticalVelocity1 = new Vector3();
+                Vector3 verticalVelocity2 = new Vector3();
+                this.GetTwoVerticalVector(originVelocity, ref verticalVelocity1, ref verticalVelocity2);
+
+                //Debug.Log("verticalVelocity1 = " + verticalVelocity1);
+                //Debug.Log("verticalVelocity2 = " + verticalVelocity2);
+
+                verticalVelocity1 = this.GetShiftPosition(verticalVelocity1, this.gameObject.GetComponent<RectTransform>().rect.width / 5);
+                verticalVelocity2 = this.GetShiftPosition(verticalVelocity2, this.gameObject.GetComponent<RectTransform>().rect.width / 5);
+
+                Debug.Log("verticalVelocity1 = " + verticalVelocity1);
+                Debug.Log("verticalVelocity2 = " + verticalVelocity2);
+
                 GameObject cloneLaserBall1 = (GameObject)Instantiate(this.gameObject,
-                                new Vector3(this.transform.position.x + this.gameObject.GetComponent<RectTransform>().rect.width, this.transform.position.y, this.transform.position.z),
+                                new Vector3(this.transform.position.x + verticalVelocity1.x, this.transform.position.y + verticalVelocity1.y, this.transform.position.z),
                                 new Quaternion(0, 0, 0, 1)
                                 );
                 cloneLaserBall1.GetComponent<Rigidbody>().velocity = Quaternion.Euler(0, 0, 10) * originVelocity;
@@ -107,7 +119,7 @@ public class LaserBall : MonoBehaviour {
                 cloneLaserBall1.transform.SetParent(this.transform.parent);
 
                 GameObject cloneLaserBall2 = (GameObject)Instantiate(this.gameObject,
-                                new Vector3(this.transform.position.x - this.gameObject.GetComponent<RectTransform>().rect.width, this.transform.position.y, this.transform.position.z),
+                                new Vector3(this.transform.position.x + verticalVelocity2.x, this.transform.position.y + verticalVelocity2.y, this.transform.position.z),
                                 new Quaternion(0, 0, 0, 1)
                                 );
                 cloneLaserBall2.GetComponent<Rigidbody>().velocity = Quaternion.Euler(0, 0, -10) * originVelocity;
@@ -144,5 +156,26 @@ public class LaserBall : MonoBehaviour {
             Destroy(this.gameObject);
             other.gameObject.GetComponent<playerCube>().IsHit(1);
         }
+    }
+
+    private Vector3 GetShiftPosition(Vector3 originVector, float distance)
+    {
+        float dist = Vector3.Distance(Vector3.zero, originVector);
+        float shiftX = originVector.x * (distance / dist);
+        float shiftY = originVector.y * (distance / dist);
+        float shiftZ = originVector.z * (distance / dist);
+        //if (originVector.x == 0)
+        //    shiftX = 0;
+        //else if (originVector.y == 0)
+        //    shiftY = 0;
+        //else if (originVector.z == 0)
+        //    shiftZ = 0;
+        return new Vector3(shiftX, shiftY, shiftZ);
+    }
+
+    private void GetTwoVerticalVector(Vector3 originVector, ref Vector3 verticalVector1, ref Vector3 verticalVector2)
+    {
+        verticalVector1 = new Vector3(0f - originVector.y, originVector.x, originVector.z);
+        verticalVector2 = new Vector3(originVector.y, 0f - originVector.x, originVector.z);
     }
 }
