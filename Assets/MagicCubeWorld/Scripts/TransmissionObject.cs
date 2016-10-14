4c627376573768bfb7f1a4f576a26b1961cfecc7
L.Guo <iml885203@gmail.com>
2016-10-14 21:39:24 +0800

    增加 傳送門cd粒子變少

--- Assets/MagicCubeWorld/Scripts/TransmissionObject.cs
+++ Assets/MagicCubeWorld/Scripts/TransmissionObject.cs
@@ -5,22 +5,37 @@ public class TransmissionObject : MonoBehaviour {
 
     public GameObject _anotherPortal;
     public GameObject _triggerTimer;
+    private TransmissionShareTimer _timer;
 
     // Use this for initialization
     void Start ()
     {
-
+        _timer = _triggerTimer.GetComponent<TransmissionShareTimer>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-
+        if(_timer == null)
+        {
+            Debug.Log("_timer null");
+            return;
+        }
+        if(_timer._timer > 0)
+        {
+            transform.FindChild("layer2").GetComponent<ParticleSystem>().maxParticles = 2;
+            this.GetComponent<ParticleSystem>().maxParticles = 2;
+        }
+        else
+        {
+            transform.FindChild("layer2").GetComponent<ParticleSystem>().maxParticles = 12;
+            this.GetComponent<ParticleSystem>().maxParticles = 24;
+        }
     }
 
     public bool CanMoveToAnotherSidePortal()
     {
-        return _triggerTimer.GetComponent<TransmissionShareTimer>().TriggerEvent();
+        return _timer.TriggerEvent();
     }
 
     public Vector3 ReturnAnotherSidePortalPosition()
