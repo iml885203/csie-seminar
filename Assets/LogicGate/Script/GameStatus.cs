cd639d4702ee9cf223eb93785d6516cb6710a0f3
L.Guo <iml885203@gmail.com>
2017-01-07 18:43:14 +0800

    增加UI簡易版

---
+++ Assets/LogicGate/Script/GameStatus.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class GameStatus : MonoBehaviour {
+
+    public string[] Statuses;
+    public string CurrentStatus = "初始化狀態為第一個";
+    public int CurrentStateIndex
+    {
+        get
+        {
+            return ArrayUtility.IndexOf(Statuses, CurrentStatus);
+        }
+    }
+    public string PrevStatus;
+    public int PrevStateIndex
+    {
+        get
+        {
+            
+            return (PrevStatus != null) ? ArrayUtility.IndexOf(Statuses, PrevStatus) : -1;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	    if(Statuses.Length == 0)
+        {
+            Debug.Log("缺少設定狀態");
+            return;
+        }
+
+        CurrentStatus = Statuses[0]; //初始化遊戲狀態
+
+    }
+	
+	public bool setStatus(string status)
+    {
+        if(ArrayUtility.IndexOf(Statuses, status) == -1)
+        {
+            Debug.Log("無法設定遊戲狀態為 " + status);
+            return false;
+        }
+
+        PrevStatus = CurrentStatus;
+        CurrentStatus = status;
+        return true;
+    }
+
+    public bool setStatus(int statusesIndex)
+    {
+        if (statusesIndex < 0 || statusesIndex > Statuses.Length - 1)
+        {
+            Debug.Log("無法設定遊戲狀態為第 " + statusesIndex + " 個狀態");
+            return false;
+        }
+
+        PrevStatus = CurrentStatus;
+        CurrentStatus = Statuses[statusesIndex];
+        return true;
+    }
+}
---
+++ Assets/LogicGate/Script/UIFlowController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIFlowController : MonoBehaviour {
+
+    public GameStatus GameStatus;
+
+    // Use this for initialization
+    void Start () {
+	    if(!validateObject())
+        {
+            return;
+        }
+        Debug.Log(GameStatus.CurrentStatus);
+        showStatusUIOnly(GameStatus.CurrentStatus);
+	}
+
+    private bool validateObject()
+    {
+        if (GameStatus == null)
+        {
+            Debug.Log("缺少GameStatus");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void backPrevStatus()
+    {
+        showStatusUIOnly(GameStatus.PrevStatus);
+    }
+	
+	public void showStatusUIOnly(string status)
+    {
+        if (!validateObject())
+        {
+            return;
+        }
+
+        Transform childUIGroup = transform.Find(status);
+        if(childUIGroup == null)
+        {
+            Debug.Log("缺少創建" + status + "狀態的UIGroup");
+            return;
+        }
+
+        if (!GameStatus.setStatus(status))
+        {
+            return;
+        }
+
+        hideStatusUIAll();
+
+        childUIGroup.gameObject.SetActive(true);
+
+    }
+
+    private void hideStatusUIAll()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
