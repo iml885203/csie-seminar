e18c9a2bb9c2faf275bf696d8e359da77dfe8b2f
ANDY <a07458666@gmail.com>
2017-04-13 18:38:51 +0800

    新增雙螢幕功能

---
+++ Assets/Sprites/DisplayMulti.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayMulti : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //設定雙螢幕的解析度
+        Screen.SetResolution(2732, 768, true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
